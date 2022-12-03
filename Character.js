1140cf8157bac73884225490ab01029c28a502e8
brandonsohxy <s10239891@connect.np.edu.sg>
2022-12-03 22:31:00 +0800

    new file:   Character.js 	modified:   about.html 	modified:   contact.html 	modified:   menu.html 	modified:   script.js 	modified:   style2.css 	modified:   style3.css 	new file:   style4.css

---
+++ Character.js
@@ -0,0 +1,15 @@
+class Character {
+    constructor(
+      name,
+      species,
+      gender,
+      role,
+    ){
+      this.name = name;
+      this.species = species;
+      this.gender = gender;
+      this.role = role;
+    }
+  };
+
+  export default Character
--- script.js
+++ script.js
@@ -5,3 +5,26 @@ toggleButton.addEventListener('click', () =>{
   naviList.classList.toggle('active');
 
 })
+
+import Character from ".Character";
+
+const mrkrabs = new Character(
+  "Mr Krabs",
+  "Crab",
+  "Male",
+  "Owner & Manager",
+);
+
+const squidward = new Character(
+  "Squidward",
+  "Giant Pacific Octopus",
+  "Male",
+  "Cashier"
+);
+
+const spongebob = new Character(
+  "Spongebob",
+  "Sea Sponge",
+  "Male",
+  "Fry cook"
+)
